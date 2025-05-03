ccad7cbd8bd8c0530b7fadde4616821a025bed86
loid-lab <fgualtieri483@gmail.com>
2025-05-03 22:35:15 +0200

    added proof of work feature

--- main.go
+++ main.go
@@ -4,75 +4,145 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
 
+const targetBits = 24
+const maxNonce = math.MaxInt64 // Max number of nonce attempts
+
 // Block represents a single block in the blockchain
 type Block struct {
-	Timestamp     int64  // Timestamp when the block was created
-	Data          []byte // Actual data stored in the block (e.g., transactions)
-	PrevBlockHash []byte // Hash of the previous block in the chain
-	Hash          []byte // Hash of the current block (calculated from its contents)
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
 }
 
-// BlockChain represents the chain of blocks
+// BlockChain stores a sequence of blocks
 type BlockChain struct {
-	blocks []*Block // Slice of pointers to blocks
+	blocks []*Block
 }
 
-// SetHash calculates and sets the hash of a block using SHA-256
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10)) // Convert timestamp to byte slice
-	// Concatenate previous hash, data, and timestamp
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers) // Generate SHA-256 hash
-
-	b.Hash = hash[:] // Set the block's hash
+// ProofOfWork contains block and target for PoW
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
 }
 
-// NewBlock creates a new block with the given data and previous block's hash
+// NewProofOfWork initializes a PoW for a block
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits)) // shift left to set difficulty
+	return &ProofOfWork{b, target}
+}
+
+// prepareData prepares the data for hashing
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+// Run executes the PoW algorithm
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+
+		// Uncomment to see hashes in real time
+		// fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\n\n")
+	return nonce, hash[:]
+}
+
+// Validate validates the PoW result
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+	return hashInt.Cmp(pow.target) == -1
+}
+
+// IntToHex converts int64 to []byte
+func IntToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 10))
+}
+
+// NewBlock creates and mines a new block
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{
-		Timestamp:     time.Now().Unix(), // Current time in Unix format
-		Data:          []byte(data),      // Convert data to bytes
-		PrevBlockHash: prevBlockHash,     // Link to previous block
-		Hash:          []byte{},          // Will be set below
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          []byte{},
+		Nonce:         0,
 	}
-	block.SetHash() // Calculate the block’s hash
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash
+	block.Nonce = nonce
 	return block
 }
 
-// AddBlock adds a new block with given data to the blockchain
+// AddBlock adds a new block to the blockchain
 func (bc *BlockChain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]   // Get the latest block
-	newBlock := NewBlock(data, prevBlock.Hash) // Create a new block linked to it
-	bc.blocks = append(bc.blocks, newBlock)    // Append the new block to the chain
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
 }
 
-// NewGenesisBlock creates the first block in the blockchain (with no predecessor)
+// NewGenesisBlock creates the first block
 func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{}) // No previous hash for the genesis block
+	return NewBlock("Genesis Block", []byte{})
 }
 
-// NewBlockchain initializes a new blockchain with the genesis block
+// NewBlockchain creates a blockchain with genesis block
 func NewBlockchain() *BlockChain {
 	return &BlockChain{[]*Block{NewGenesisBlock()}}
 }
 
-// main is the entry point of the program
+// main runs the app
 func main() {
-	bc := NewBlockchain() // Initialize blockchain
-
-	// Add two more blocks
+	bc := NewBlockchain()
 	bc.AddBlock("Send 1 BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
-	// Print out each block's details
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW valid: %v\n", pow.Validate())
 		fmt.Println()
 	}
 }
